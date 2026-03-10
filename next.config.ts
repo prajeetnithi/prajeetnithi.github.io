e8d92fa438f8071b4b047ea7b0b2b13a4ca82cdd
Prajeet Nithi K <prajeetnithi@karunya.edu.in>
2026-03-10 21:56:29 +0530

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -2,11 +2,10 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   output: "export",
-  basePath: "",
-  assetPrefix: "/",
   images: {
     unoptimized: true
-  }
+  },
+  trailingSlash: true
 };
 
 export default nextConfig;
